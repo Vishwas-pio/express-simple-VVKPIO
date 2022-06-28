41af23a098e1a8eaf89a63d1ad5b07e29fd3ab9c
VvkPIO <vishwas.kalaskar@programmers.io>
2022-06-28 15:34:46 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+  const express = require('express');
 const app = express();
 const port = 3010;
 const path = require('path');
+const members = [
+  {
+    Id : 1,
+name : 'string',
+cost : 15.30 ,
+description : 'string'   
 
+  }
+] 
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+app.get('/api/members', (req, res) => {
+  res.json(members);
 });
 
 app.listen(port, () => {
